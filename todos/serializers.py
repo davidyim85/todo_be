daef6fa1a83bb361c1272567acea03abec5dfa97
yimd85 <yimd85@gmail.com>
2023-09-26 22:20:49 -0400

    initial commit

---
+++ todos/serializers.py
@@ -0,0 +1,24 @@
+from .models import Todo
+from rest_framework import serializers
+from django.contrib.auth.models import User, Group
+
+## Class TodoSerializer is a subclass of serializers.HyperlinkedModelSerializer.
+## For serializing and deserializing data into representations such as json.
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    ## Meta class is for configuring the TodoSerializer class.
+    class Meta:
+        # model to serialize
+        model = Todo
+        # fields to show in json
+        fields = ('id', 'subject', 'details')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
---
+++ todos/views.py
@@ -0,0 +1,38 @@
+from .models import Todo
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TodoSerializer, UserSerializer, GroupSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication 
+
+
+# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    ## The Main Query for the index route
+    queryset = Todo.objects.all()
+    # The serializer class for serializing output
+    serializer_class = TodoSerializer
+    # optional permission class set permission level
+    permission_classes = [AllowAny]
+    # authentication_classes = (JWTAuthentication,)
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [AllowAny]
+    # authentication_classes = (JWTAuthentication,)
